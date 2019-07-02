6bf9d54bca345555667847e370b12e19c4127c4a
juancruzgardey <juancruzgardey@hotmail.com>
2019-07-02 07:59:54 -0300

    fix the begin and the end of recording events

--- src/content_scripts/recorder.js
+++ src/content_scripts/recorder.js
@@ -9,9 +9,9 @@ ScreenRecorder.prototype.toggleRecording = function () {
         modal.show();
     }
     else {
+        this.stopRecording();
         this.recording = false;
         browser.runtime.sendMessage({"message": "stop"});
-
     }
 }
 
@@ -20,32 +20,32 @@ ScreenRecorder.prototype.startRecording = function () {
     this.recording = true;
     this.screencastId = Math.random().toString(36).substring(2, 15) + "-" + Date.now();
     browser.runtime.sendMessage({"message": "start"});
-}
-
-ScreenRecorder.prototype.setUp = function () {
     const me = this;
-    rrweb.record({
+    this.stopRecording = rrweb.record({
         emit(event) {
-            if (me.recording) {
-                me.events.push(event);
-            }
+            me.events.push(event);
         },
     });
 
-// this function will send events to the backend and reset the events array
+};
+
+ScreenRecorder.prototype.setUp = function () {
+    // this function will send events to the backend and reset the events array
+    const me = this;
     function save() {
         if (me.events.length > 0) {
             console.log('events pushed:', me.events);
-            browser.runtime.sendMessage({"message":"save", "data":{"events": me.events, "screencastName": me.screencastName, "id": me.screencastId}});
+            browser.runtime.sendMessage({"message":"save", "data":{"events": me.events, "screencastName": me.screencastName,
+                    "screencastId": me.screencastId, "url": document.location.href}});
             me.events = [];
         }
     }
-    setInterval(save, 5 * 1000);
-}
+    setInterval(save, 10 * 1000);
+};
 
 var screenRecorder = new ScreenRecorder();
-var modal = new RecorderModal(screenRecorder);
 screenRecorder.setUp();
+var modal = new RecorderModal(screenRecorder);
 
 browser.runtime.onMessage.addListener((request, sender) => {
     screenRecorder.toggleRecording();
